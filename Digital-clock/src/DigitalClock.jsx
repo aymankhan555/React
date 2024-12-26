34f9b96d9d4a10bb5bba6be965b2bac620e8b2d2
Md AYMAN <khanayman555@gmail.com>
2024-12-26 15:13:34 +0600

    Added digital clock program

---
+++ Digital-clock/src/DigitalClock.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+
+function DigitalClock() {
+    const [time, setTime] = useState(new Date());
+    useEffect(() => {
+        const timeId = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+        console.log('h');
+        return () => clearInterval(timeId);
+    }, []);
+    function render() {
+        let hours = time.getHours();
+        const minutes = time.getMinutes();
+        const seconds = time.getSeconds();
+        const meridian = hours>=12 ? "PM" : "AM";
+        hours = hours % 12 || 12;
+        return (`${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridian}`);
+    }
+    function padZero(time)
+    {
+        return time<10? '0'+time: time;
+    }
+    return (
+        <div className="clock-container">
+            <div className="clock">
+                <span>{render()}</span>
+            </div>
+        </div>
+    );
+}
+export default DigitalClock;
