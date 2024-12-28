797672abf4726fd92670f5612d3d7c338fae331a
Md AYMAN <khanayman555@gmail.com>
2024-12-28 20:25:13 +0600

    modified the html structure and some css

--- Tic-Tac-Toe/src/TicTacToe.jsx
+++ Tic-Tac-Toe/src/TicTacToe.jsx
@@ -1,26 +1,32 @@
 
 function TicTacToe() {
+    let board=["X","O","","X","O","","X","O",""];
+  
 
     return (
         <div className="container">
             <h1>Tic-Tac-Toe</h1>
-            <div className="row-1">
-                <button className="moves"></button>
-                <button className="moves"></button>
-                <button className="moves"></button>
+            <div className="row">
+                {board.slice(0,3).map((object,index)=>(
+                    <button key={index} className="moves"
+                    disabled={object!==""}>{object}</button>
+                ))}
             </div>
-            <div className="row-2">
-                <button className="moves"></button>
-                <button className="moves"></button>
-                <button className="moves"></button>
+            <div className="row">
+                {board.slice(3,6).map((object,index)=>(
+                    <button key={index+6} className="moves"
+                    disabled={object!==""}>{object}</button>
+                ))}
             </div>
-            <div className="row-3">
-                <button className="moves"></button>
-                <button className="moves"></button>
-                <button className="moves"></button>
+            <div className="row">
+                {board.slice(6,9).map((object,index)=>(
+                    <button key={index+6} className="moves"
+                    disabled={object!=""}>{object}</button>
+                ))}
             </div>
 
             <button className="reset-button">Reset</button>
+            
         </div>
     );
 }
