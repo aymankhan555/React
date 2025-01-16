d39d760ef87f96280594f8b9cb4a72ab1e004e88
Md AYMAN <khanayman555@gmail.com>
2025-01-16 21:33:03 +0600

    added notification using toast

--- Todo-list-program/src/TodoList.jsx
+++ Todo-list-program/src/TodoList.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function TodoList() {
 	const [tasks, setTasks] = useState(["take shower", "play games", "do chores"]);
 	const [newTask, setNewTask] = useState("");
@@ -7,36 +9,38 @@ function TodoList() {
 		setNewTask(e.target.value);
 	}
 	function addList() {
-		if(newTask.trim()!=="")
-		{
-			setTasks(t=>[...t, newTask]);
+		if (newTask.trim() !== "") {
+			setTasks(t => [...t, newTask]);
 			setNewTask("");
+			toast.success('List is added');
 		}
-	
+
 	}
 	function deleteList(index) {
-		const updatedTasks = tasks.filter((_,i)=>i!==index);
+		const updatedTasks = tasks.filter((_, i) => i !== index);
 		setTasks(updatedTasks);
+		toast.info('List is deleted');
+
 	}
 	function moveUp(index) {
 
-		if(index>0)
-		{
+		if (index > 0) {
 			const updatedTasks = [...tasks];
-			[updatedTasks[index],updatedTasks[index-1]]=[updatedTasks[index-1],updatedTasks[index]];
+			[updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
 			setTasks(updatedTasks);
 		}
 	}
 	function moveDown(index) {
-		if(index<tasks.length-1)
-			{
-				const updatedTasks = [...tasks];
-				[updatedTasks[index],updatedTasks[index+1]]=[updatedTasks[index+1],updatedTasks[index]];
-				setTasks(updatedTasks);
-			}
+		if (index < tasks.length - 1) {
+			const updatedTasks = [...tasks];
+			[updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
+			setTasks(updatedTasks);
+		}
 	}
 	return (
+
 		<div className="to-do-list">
+			<ToastContainer />
 			<h1>To-Do-List</h1>
 			<div>
 				<input type="text" placeholder="Enter a task..."
